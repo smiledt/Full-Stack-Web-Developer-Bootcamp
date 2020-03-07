4e4955c3aa2cbf017642d5e8863c4fe72b8ce4f9
smiledt <dereksmilees@gmail.com>
2020-03-07 13:37:29 -0600

    Moving to laptop

---
+++ python_lesson1/strings.py
@@ -0,0 +1,12 @@
+# STRINGS
+
+mystring = 'abcdefg'
+
+print(mystring)
+
+# Python supports both indexing and negative (from the end) indexing
+print(mystring[3])
+print(mystring[-1])
+
+# Slicing
+print(mystring[2:]) #Everything from the second char onwards
